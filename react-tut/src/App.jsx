8832242a92a249eece5e7d591adccd7cde7e27ff
= <=>
2025-02-18 22:35:58 +0530

    axios

--- react-tut/src/App.jsx
+++ react-tut/src/App.jsx
@@ -1,14 +1,42 @@
-import React from 'react';
-import Nav from './components/Nav';
-import { Routing } from './util/Routing';
-import UserDetail from './components/UserDetail';
-function App() {
+import React from 'react'
+import axios from 'axios'
+const App = () => {
+    const getProducts = () => {
+        console.log('Function called')
+        const api = 'https://fakestoreapi.com/products'
+        axios
+            .get(api).then(products => {
+                console.log(products)
+
+            })
+            .catch((err) => console.log(err))
+    }
+
+    const addProducts = () => {
+        console.log('Post function called');
+        const api = 'https://fakestoreapi.com/products'
+        axios
+            .post(api, {
+                title: 'Juhi Chawala',
+                price: 13.5,
+                description: 'lorem ipsum set',
+                image: 'https://i.pravatar.cc',
+                category: 'electronic'
+            })
+            .then(() => {
+                console.log('Post Done')
+            })
+            .catch((err) => {
+                console.log("Got Error")
+            })
+    }
     return (
-        <>
-            <Nav />
-            <Routing />
-        </>
-    );
+
+        <div>
+            {addProducts()}
+            {getProducts()}
+        </div>
+    )
 }
 
-export default App;
+export default App
