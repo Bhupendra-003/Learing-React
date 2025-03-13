f736a8ac8844153566d45441c192b944f20697c3
Bhupendra <bhupendra.raj003@gmail.com>
2025-03-13 19:34:57 +0530

    reset

--- react-tut/src/App.jsx
+++ react-tut/src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import Nav from './components/Nav'
-import Routing from './util/Routing'
 
 function App() {
     return (
-        <div className='bg-[#1F1F1F] w-full h-fit'>
-            <Nav />
-            <Routing />
+        <div>
+            Hello
         </div>
     )
 }
